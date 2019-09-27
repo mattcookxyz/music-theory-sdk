3e232f8d598da9cc68d4a501ff3be27dfa065b3c
Matt Cook <matt@mattcook.xyz>
2019-09-26 19:48:15 -0500

    Another fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 declare module "music-theory-toolkit" {
     declare class Note {
-        static random(alpha: boolean = false, flatSharpFilter: boolean|string = false): string|number;
-        static numNoteToAlpha(numNote: number, flatSharpFilter: boolean|string = false): string;
+        static random(alpha: boolean, flatSharpFilter?: boolean|string): string|number;
+        static numNoteToAlpha(numNote: number, flatSharpFilter?: boolean|string): string;
         static alphaNoteToNumeric(alphaNote: string): number;
-        static applyInterval(root: string|number, interval: number, constrainToBaseOctave: boolean = true, flatSharpFilter: boolean|string = false): number|string;
+        static applyInterval(root: string|number, interval: number, constrainToBaseOctave?: boolean, flatSharpFilter?: boolean|string): number|string;
         toAlphaDict: {
             [index: number]: string;
         }
