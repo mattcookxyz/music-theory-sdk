8b7f3f6876bbb18ad6d2716319f25f229abb9350
Matt Cook <matt@mattcook.xyz>
2019-09-27 19:16:12 -0500

    All type defs

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,11 +14,18 @@ declare module "music-theory-toolkit" {
         numChord: number[];
     }
 
-    declare class Note {
-        static random(alpha: boolean, flatSharpFilter: boolean|string = false): string|number;
-        static numNoteToAlpha(numNote: number, flatSharpFilter: boolean|string = false): string;
+    interface IScale {
+        difficulty: number;
+        mode: string;
+        category: string;
+        structure: number[];
+    }
+
+    class Note {
+        static random(alpha: boolean, flatSharpFilter?: boolean|string): string|number;
+        static numNoteToAlpha(numNote: number, flatSharpFilter?: boolean|string): string;
         static alphaNoteToNumeric(alphaNote: string): number;
-        static applyInterval(root: string|number, interval: number, constrainToBaseOctave: boolean = true, flatSharpFilter: boolean|string = false): number|string;
+        static applyInterval(root: string|number, interval: number, constrainToBaseOctave?: boolean, flatSharpFilter?: boolean|string): number|string;
         toAlphaDict: {
             [index: number]: string;
         }
@@ -27,13 +34,39 @@ declare module "music-theory-toolkit" {
         }
     }
 
-    declare class Chord {
-        static random(difficulty: number = 4, onlyThisDifficulty: boolean = false, constrainToBaseOctave: boolean = true, flatSharpFilter: boolean|string = false): IChord;
-        static randomQuality(difficulty: number = 5, onlyThisDifficulty: boolean = false): IChordQuality;
+    class Chord {
+        static random(difficulty?: number, onlyThisDifficulty?: boolean, constrainToBaseOctave?: boolean, flatSharpFilter?: boolean|string): IChord;
+        static randomQuality(difficulty?: number, onlyThisDifficulty?: boolean): IChordQuality;
+        static allQualitiesWithDifficulty(difficulty?: number, onlyThisDifficulty?: boolean): IChordQuality[];
+        static applyStructureToRoot(root: number|string, structure: number[], constrainToBaseOctave?: boolean, alpha?: boolean, flatSharpFilter?: boolean|string): number[]|string[];
+        static allQualities(): {
+            [index: string]: IChordQuality;
+        };
+    }
+
+    class Set {
+        static getNumericNoteSet(root?: number|string): number[];
+        static random(): number[];
+        static getAlphaNoteSet(root?: number|string, flatSharpFilter?: boolean|string): string[];
+        static alphaSetToNumeric(alphaNoteSet: string[]): number[];
+        static numericSetToAlpha(numNoteSet: number[], flatSharpFilter?: boolean|string): string[];
+        static applyStructureToRoot(root: number|string, structure: number[], constrainToBaseOctave?: boolean, alpha?: boolean, flatSharpFilter?: string|boolean): string[]|number[];
+    }
+
+    class Scale {
+        static getScaleFromRoot(root: string|number, structure: number[], constrainToBaseOctave?: boolean): number[];
+        static random(difficulty?: number, onlyThisDifficulty?: boolean): number[];
+        static allScalesWithDifficulty(difficulty?: number, onlyThisDifficulty?: boolean): number[][];
+        static applyStructureToRoot(root: string|number, structure: number[], constrainToBaseOctave?: boolean): number[];
+        static allScales(): {
+            [index: string]: IScale;
+        }
     }
 
     export {
         Note,
-        Chord
+        Chord,
+        Set,
+        Scale
     }
 }
