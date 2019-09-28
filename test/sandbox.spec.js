4c426c74eca64a667a7438b18d5c7974f1776fdf
Matt Cook <matt@mattcook.xyz>
2019-09-28 18:13:27 -0500

    Placeholder test

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -6,16 +6,13 @@ import { expect } from 'chai';
 describe('Note Class', () => {
   describe('Method: random', () => {
     it('Should properly generate a random note', () => {
-      for (let i = 0; i < 100; i++) {
-        new Note()
-          .toAlpha()
-          .toNum()
-          .calculate()
-          .transpose(12)
-          .calculate()
-          .transpose(-35);
-      }
-      expect(1).to.equal(1);
+      expect(new Note()
+      .toAlpha()
+      .toNum()
+      .calculate()
+      .transpose(12)
+      .calculate()
+      .transpose(-35)).to.not.throw;
     });
   });
 });
