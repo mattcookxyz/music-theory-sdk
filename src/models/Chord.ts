7a41b3f90f137c5ab9e4ff495a896ce84e434ac4
Matt Cook <matt@mattcook.xyz>
2019-09-28 21:47:23 -0500

    More

---
+++ src/models/Chord.ts
@@ -0,0 +1,11 @@
+import { Note } from './Note';
+
+export class Chord {
+
+  public notes: Note[];
+  public alpha: boolean;
+
+  constructor() {
+
+  }
+}
--- src/models/Set.ts
+++ src/models/Set.ts
@@ -4,7 +4,6 @@ import { sortBy, shuffle } from 'lodash';
 export class Set {
 
   public notes: Note[];
-  public alpha: boolean;
   public ordered: boolean;
   public order: string|null;
 
