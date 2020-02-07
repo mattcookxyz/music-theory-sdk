3c8e61591eef0ba5c0d57834fe3b3730ac7d99e9
Matt Cook <matt@mattcook.xyz>
2020-02-06 19:21:46 -0600

    Fix regex to use portable unicode matching

--- src/util/regex.ts
+++ src/util/regex.ts
@@ -1,9 +1,9 @@
 // Match chords filtered to only contain a flat or sharp (i.e. BbMaj7)
-export const validChordWithFilter = /^[A-Ga-g][#|b]?[A-Za-z\°\ø\+]*[0-9]*[b|#]*[0-9]{0,2}$/;
+export const validChordWithFilter = /^[A-Ga-g][#|b]?[A-Za-z\u{00B0}\u{00F8}\+]*[0-9]*[b|#]*[0-9]{0,2}$/u;
 
 // Match chords that may not be filtered to contain only a flat or sharp (i.e. A#/BbMaj7)
 // tslint:disable-next-line
-export const validChordWithoutFilter = /^[A-Ga-g][#|b]?(\/[A-Ga-g][#|b]?)?[A-Za-z\°\ø\+]*[0-9]*[b|#]*[0-9]{0,2}$/;
+export const validChordWithoutFilter = /^[A-Ga-g][#|b]?(\/[A-Ga-g][#|b]?)?[A-Za-z\u{00B0}\u{00F8}\+]*[0-9]*[b|#]*[0-9]{0,2}$/u;
 
 // Valid note patterns
 // tslint:disable-next-line
